a380aa6f1b2b0d349b903c40864eab7971c5a949
sintaanjelina <sinta.anjelina3930@gmail.com>
2018-08-22 12:32:44 +0700

    FIX-02 MAKING TODO
    todo model, repository, controller

--- src/main/java/com/example/postgresdemo/repository/UserRepository.java
+++ src/main/java/com/example/postgresdemo/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.postgresdemo.repository;
 
 import com.example.postgresdemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
